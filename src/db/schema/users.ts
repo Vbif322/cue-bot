abd8020c48a9dc348df1cf2adf33fb04fecf74ea
Vbif322 <hxdfue@yandex.ru>
2025-12-06 13:50:37 +0300

    upd tscofig + add columns to user

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,9 +1,15 @@
-import { uuid, varchar } from "drizzle-orm/pg-core";
+import { date, uuid, varchar } from "drizzle-orm/pg-core";
 import { prodSchema } from "../schemaHelpers.js";
 
 export const users = prodSchema.table("users", {
   id: uuid("id").primaryKey().defaultRandom(),
   telegram_id: varchar({ length: 255 }).notNull().unique(),
+  username: varchar({ length: 255 }).notNull(),
+  phone: varchar({ length: 20 }),
+  email: varchar({ length: 255 }),
+  name: varchar({ length: 50 }),
+  surname: varchar({ length: 100 }),
+  birthday: date(),
   role: varchar({ enum: ["user", "admin"] })
     .notNull()
     .default("user"),
