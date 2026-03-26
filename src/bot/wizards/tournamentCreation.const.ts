a18f3ba02dd450015d054af5212ff02df5ff14e8
Nik Moro <74981818+Rous-well-works@users.noreply.github.com>
2026-03-26 04:19:19 +0300

    feat: allocation of consts

---
+++ src/bot/wizards/tournamentCreation.const.ts
@@ -0,0 +1,12 @@
+export const CREATION_STEPS = [
+  'name',
+  'date',
+  'venue',
+  'discipline',
+  'format',
+  'maxParticipants',
+  'winScore',
+  'tables',
+] as const;
+
+export const STEPS_COUNT = CREATION_STEPS.length;
